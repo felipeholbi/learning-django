3d47d1f730456191ec0ec2b92862bb8f26382688
felipeholbi <4758@holbertonstudents.com>
2023-01-28 20:33:54 -0500

    URL Routes and views base

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Pet
+from django.http import Http404
+
+def home(request):
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {
+        'pets': pets,
+    })
+
+def pet_detail(request, pet_id):
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404(f'The pet with id {pet_id} not match')
+    return render(request, 'pet_detail.html', {
+        'pet': pet,
+    })
